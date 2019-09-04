321500aed3a32b71afac1a4baa2e89a32adb9c11
Jonas Saldanha <jonassaldanha218@gmail.com>
2019-09-04 08:08:55 -0300

    conexao ao db

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('BD', 'foxish');
+
+    $conn = mysqli_connect(HOST, USER, PASSWORD, BD) or die ("No possible conectar ao bd");
+?>
