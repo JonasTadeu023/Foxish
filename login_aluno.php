31c2f86fc210adc18492a0d46c2af95a64cb24ac
DaddySyres <darivafilho@gmail.com>
2019-09-06 18:45:59 -0300

    adicionando o login de aluno

--- login_aluno.php
+++ login_aluno.php
@@ -33,40 +33,30 @@
 <div class="conteiner">
   <div class="container">
     <div class="row">
-        <div class="col s12 ">
+        <div class="col s12 m7">
             <h5>Login</h5>
             <form action="">
+            <div class="conteiner">
             <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
+                  <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">First Name</label>
+                  </div>
+                  <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Last Name</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                  </div>
+                </div>
                 <button class="waves-effect waves-light btn" type="submit">Enviar <i class="material-icons right">send</i></button>
             </form>
         </div>
+      </div>
     </div>
   </div>
     <footer class="page-footer blue-grey darken-2">
