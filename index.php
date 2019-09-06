ed032084c77f9cd3dd89dc002d1d68f5a1914269
DaddySyres <darivafilho@gmail.com>
2019-09-06 10:41:37 -0300

    Melhorando o index e o icone foxish

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Início</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/css?family=Lexend+Giga&display=swap" rel="stylesheet"> 
+    <style>
+      a{
+        color: rgba(251, 176, 45, 1);
+      }
+
+      a:hover{
+        color: rgba(251, 176, 45, 1); 
+      }
+      p{
+        font-size: 22px;
+      }
+    </style>
+</head>
+<body>
+<div class="navbar-fixed">
+  <nav class="blue-grey darken-4" style="opacity:0.925;">
+    <div class="nav-wrapper blue-grey darken-4" style="opacity:0.90;">
+        <a class="logo-conteiner" ><img src="imagens/foxish-original-pure_color.png" style="width: 50px; margin-left: 80px; margin-top:3  px;"></a>
+        <a href="#" class="brand-logo .foda" style="margin-left: 10px;font-family: 'Lexend Giga', sans-serif;color: rgba(251, 176, 45, 1);">Foxish</a>      
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class="" style="font-family: 'Lexend Giga', sans-serif;font-size:18px;" href="sass.html">Sass</a></li>
+        <li><a class="" style="font-family: 'Lexend Giga', sans-serif;font-size:18px;" href="badges.html">Components</a></li>
+        <li><a class="" style="font-family: 'Lexend Giga', sans-serif;font-size:18px;" href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+<div class="conteiner">
+  <div class="container">
+
+  <h5>Example Promotion Table</h5>
+          <p>If we want 3 divs that are equal size, we define the divs with a width of 4-columns, as 4+4+4 nicely adds up to 12. Inside those divs, we can put our content. Take our front page content for example. We've modified it slightly for the sake of this example.</p>
+          <div class="row">
+            <div class="col s4">
+              <div class="center promo promo-example">
+                <i class="material-icons">flash_on</i>
+                <p class="promo-caption">Speeds up development</p>
+                <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+              </div>
+            </div>
+            <div class="col s4">
+              <div class="center promo promo-example">
+                <i class="material-icons">group</i>
+                <p class="promo-caption">User Experience Focused</p>
+                <p class="light center">By utilizing elements and principles of Material Design, we were able to create a framework that focuses on User Experience.</p>
+              </div>
+            </div>
+            <div class="col s4">
+              <div class="center promo promo-example">
+                <i class="material-icons">settings</i>
+                <p class="promo-caption">Easy to work with</p>
+                <p class="light center">We have provided detailed documentation as well as specific code examples to help new users get started.</p>
+              </div>
+            </div>
+          </div>
+
+        <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>wdfwd
+  <br>weefeedewdwded
+  <br>wefewwe
+  <br>weddw
+  <br>weed
+  <br>dwewe
+  <br><brd>dwed
+  <br>ewd
+  <br>
+  <br>
+  </div>
+  <footer class="page-footer blue-grey darken-2">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text" style="font-size:16px;">Footer Content</h5>
+                <p class="grey-text text-lighten-4" style="font-size:14px;">You can use rows and columns here to organize your footer content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright blue-grey darken-4" >
+            <div class="container">
+            © 2019 Copyright Black Softweres
+            <a class="grey-text text-lighten-4 right" href="#!">Futuros projetos!</a>
+            </div>
+          </div>
+        </footer>
+</div>
+</body>
+</html>
