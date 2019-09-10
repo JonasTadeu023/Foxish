aa3227a4763ffeb58f7d4caf169678a9008b64c3
joao_danilo <jddiedrich@gmail.com>
2019-09-10 19:11:31 -0300

    varios bagui

--- includes/includes_login_professor.php
+++ includes/includes_login_professor.php
@@ -12,9 +12,10 @@ $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 $query = "SELECT prof_id, prof_email from prof where prof_email = '{$email}' and prof_senha = md5('{$senha}')";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $row = mysqli_num_rows($result);
+$dados = mysqli_fetch_assoc($result);
 
 if ($row == 1) {
-    $_SESSION['usuario'] = $email;
+    $_SESSION['usuario'] = ;
     header('location: perfis/perfil_professor.php');
     exit();
 } else {
