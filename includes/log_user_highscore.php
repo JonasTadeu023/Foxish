6851e90b855d5fb372c1857875c77e877629dd24
DaddySyres <darivafilho@gmail.com>
2019-09-11 08:51:36 -0300

    metendo o quiz no trilhos

--- includes/log_user_highscore.php
+++ includes/log_user_highscore.php
@@ -1,42 +1,55 @@
 <?php
-    session_start();
-    include_once "question_array.php";
-    if(isset($_POST['submit'])){
-        for ($i=1; $i < count($_POST); $i++){
-            $name_concatenation = "quest$i";
-            if (true){
-                $post_array[$i] = mysqli_real_escape_string($conn ,$_POST["$name_concatenation"]);
-            }
-            else{
-                $post_array[$i] = false;
-            }
-        }
+session_start();
+include_once "dbh.php";
 
-
-        $jogador_recorde = 0;
-        for ($i=1; array_key_exists($i, $reg) == true; $i++) {
-            if($post_array[$i] == $reg[$i][5]){
-             
-               $jogador_recorde++;
-            }
-        }
-        exit();         
-        $id = $_SESSION['jogador_id'];
-        $query = "SELECT jogador_recorde FROM `jogador` WHERE `id` = '$id';";
-        $result = mysqli_fetch_assoc(mysqli_query($conn, $query));
-        if($jogador_recorde > $result['jogador_recorde']){
-            $query = "UPDATE jogador SET jogador_recorde = $jogador_recorde ;";
-            mysqli_query($conn, $query);
-            header("Location: leaderboards.php");
-            exit();
-        }
-        else{
-            header("Location: leaderboards.php");
-            exit();
+if (isset($_POST['submit'])) {
+    for ($i = 1; $i < count($_POST); $i++) {
+        $name_concatenation = "quest$i";
+        if (true) {
+            $post_array[$i] = mysqli_real_escape_string($conn, $_POST["$name_concatenation"]);
+        } else {
+            $post_array[$i] = false;
         }
     }
-    else{
-        header("Location: ../quiz.php");
+
+    $jogador_recorde = 0;
+    for ($i = 1; array_key_exists($i, $_SESSION) == true; $i++) {
+        if ($post_array[$i] == $_SESSION[$i][5]) {
+
+            $jogador_recorde++;
+        }
+    }
+    exit();
+    if ($joagdor_record == 22) {
+        $fluencia = 'GODLIKE';
+        $nivel = 5;
+    } elseif ($joagdor_record > 19 && $joagdor_record < 22) {
+        $fluencia = 'Fluente';
+        $nivel = 4;
+    } elseif ($joagdor_record > 15 && $joagdor_record < 19) {
+        $fluencia = 'Mediano';
+        $nivel = 3;
+    } elseif ($joagdor_record > 7 && $joagdor_record < 15) {
+        $fluencia = 'Aprendiz';
+        $nivel = 2;
+    } else {
+        $fluencia = 'Iniciante';
+        $nivel = 1;
+    }
+
+    $id = $_SESSION['usuario_id'];
+    $query = "SELECT aluno_nivel,aluno_fluencia FROM `aluno` WHERE `id` = '$id';";
+    $result = mysqli_fetch_assoc(mysqli_query($conn, $query));
+    if ($nivel > $result['aluno_nivel']) {
+        $query = "UPDATE aluno SET aluno_nivel = (int)$nivel, aluno_fluencia = '$fluencia' ;";
+        mysqli_query($conn, $query);
+        header("Location: perfis/perfil_aluno.php");
+        exit();
+    } else {
+        header("Location: perfis/perfil_aluno.php");
         exit();
     }
-?>
+} else {
+    header("Location: ../quiz.php");
+    exit();
+}
